b62a146d88b8cf5c34e11461fe855838d08ab1c7
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-08-27 14:27:47 -0400

    drafted service-worker

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+// when we click our extension in jigsaw tab, this event listener will be fired and will pass instance of tab (tab we have opened)
+chrome.action.onClicked.addListener(tab => {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: () =>{
+            alert("Hello from my extension!");
+        }
+    })
+})
